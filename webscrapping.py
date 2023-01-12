5e6fcf961dff7a3d76f76a0b17cde18ca01056b0
Hrutik Mehta <hrutikmehta@icloud.com>
2023-01-11 20:07:56 -0500

    Attempt to get Requests to work with Indeed

--- webscrapping.py
+++ webscrapping.py
@@ -4,44 +4,35 @@ import pandas as pd
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service  # Used to set Chrome location
+from selenium.webdriver.chrome.options import Options  # Used to add aditional settings (ex. run in background)
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
-           "Accept-Encoding": "gzip, deflate",
-           "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT": "1",
-           "Connection": "close", "Upgrade-Insecure-Requests": "1"}
-
-
-def get_current_url(url, job_title, location):
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get(url)
-    time.sleep(3)
-    driver.find_element_by_xpath('//*[@id="text-input-what"]').send_keys(job_title)
-    time.sleep(3)
-    driver.find_element_by_xpath('//*[@id="text-input-where"]').clear()
-    time.sleep(3)
-    driver.find_element_by_xpath('//*[@id="text-input-where"]').send_keys(location)
-    time.sleep(3)
-    driver.find_element_by_xpath('/html/body/div').click()
-    time.sleep(3)
-    try:
-        driver.find_element_by_xpath('//*[@id="jobsearch"]/button').click()
-    except:
-        driver.find_element_by_xpath('//*[@id="whatWhereFormId"]/div[3]/button').click()
-    current_url = driver.current_url
-
-    return current_url
-
-
-current_url = get_current_url('https://ca.indeed.com/', 'Data Scientist', "Toronto, ON")
-print(current_url)
+HEADERS = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',}
+url = 'https://ca.indeed.com/jobs?q=Data+Scientist&l=Toronto&start=0'
 
 
 def scrape_job_details(url):
-    resp = requests.get(url, headers=HEADERS)
-    content = BeautifulSoup(resp.content, 'lxml')
-
+    resp = requests.get(url, HEADERS)
+    content = BeautifulSoup(resp.text, 'html.parser')
+    print(content)
     jobs_list = []
+    for post in content.find_all("a", class_="jobtitle turnstileLink"):
+        try:
+            data = {
+                "job_title": post.select('.jobTitle')[0].get_text().strip(),
+                "company": post.select('.companyName')[0].get_text().strip(),
+                "rating": post.select('.ratingNumber')[0].get_text().strip(),
+                "location": post.select('.companyLocation')[0].get_text().strip(),
+                "date": post.select('.date')[0].get_text().strip(),
+                "job_desc": post.select('.job-snippet')[0].get_text().strip()
+
+            }
+        except IndexError:
+            continue
+        jobs_list.append(data)
     for post in content.select('.job_seen_beacon'):
         try:
             data = {
@@ -61,4 +52,4 @@ def scrape_job_details(url):
     return dataframe
 
 
-print(scrape_job_details(current_url))
+print(scrape_job_details(url))
